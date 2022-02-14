4b1c1ae8e9959d83370cd33b0ef9d3213559c2cf
Antoine07 <antoine.lucsko@gmail.com>
2022-02-14 11:24:53 +0100

    os

---
+++ Node_Express_Mongo/semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Examples/app/index.js
@@ -0,0 +1,7 @@
+
+const dayjs = require('dayjs');
+
+
+
+const d = dayjs('2018-08-08') ;
+console.log(dayjs().format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A'))
---
+++ Node_Express_Mongo/semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Examples/moduleNatifs/index.js
@@ -0,0 +1,4 @@
+// Module natif de Node.js
+const os = require('os');
+
+console.log(os.userInfo());
